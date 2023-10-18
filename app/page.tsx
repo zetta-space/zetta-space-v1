9218adc3ccb5e7c22242a7af523ca11e8a4e6f59
Harshana Wijesinghe <harshawije99@gmail.com>
2023-10-18 14:32:30 +0530

    project init

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-slate-950 w-full h-screen flex justify-center items-center">
+      <div className="flex-wrap">
+        <p className="text-white font-normal text-base capitalize mb-1">Zetta Space</p>
+        <hr />
+        <h2 className="capitalize text-white font-bold text-6xl mt-4">Coming Soon...</h2>
+      </div>
+    </main>
+  );
+}
