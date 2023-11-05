12d33d468c8709c25a6221d9fe04a39437ad539f
Harshana Wijesinghe <harshawije99@gmail.com>
2023-11-05 10:59:46 +0530

    init development

---
+++ app/experimental/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ExperimentalPage() {
+  return (
+    <div>
+      <h2>Nothing Found Here </h2>
+    </div>
+  );
+}
+
+export default ExperimentalPage;
---
+++ components/SideBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+function SideBar() {
+  return (
+    <>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
+
+export default SideBar;
