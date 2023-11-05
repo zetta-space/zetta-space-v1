ae8abc82a41a1d618f45db9b8af2cd4daeae5e6f
Harshana Wijesinghe <harshawije99@gmail.com>
2023-11-05 11:11:21 +0530

    navbar completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+import SideBar from "@/components/SideBar";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="bg-slate-950 w-full h-screen flex justify-center items-center">
-      <div className="flex-wrap">
-        <p className="text-white font-normal text-base capitalize mb-1">Zetta Space</p>
-        <hr />
-        <h2 className="capitalize text-white font-bold text-6xl mt-4">Coming Soon...</h2>
-      </div>
+    <main className="bg-white w-full">
+      <nav className="w-full px-10 py-5 bg-white shadow-md fixed top-0 flex flex-row justify-between items-center">
+        <div>
+          <SideBar />
+        </div>
+        <span>
+          <h2 className="text-5xl font-bold capitalize">Z.</h2>
+        </span>
+        <div className="flex flex-row justify-start items-center gap-3">
+          <Link href={"/"} className="px-4 py-2.5 bg-blue-500 text-white font-medium text-base rounded-full">Create account.</Link>
+        </div>
+      </nav>
+      <section className="mt-12 py-32 px-4">
+        <div>welcome explore</div>
+        <div>slide view</div>
+        <div>
+          <section>cards</section>
+        </div>
+      </section>
     </main>
   );
 }
