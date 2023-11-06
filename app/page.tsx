912c1f67a470d7fce04da064a2cdf93230786020
Harshana Wijesinghe <harshawije99@gmail.com>
2023-11-07 00:03:10 +0530

    article page developing

--- app/page.tsx
+++ app/page.tsx
@@ -9,22 +9,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="bg-white w-full">
-      <nav className="w-full px-10 py-5 bg-white shadow-md fixed top-0 flex flex-row justify-between items-center">
-        <div>
-          <SideBar />
-        </div>
-        <span>
-          <h2 className="text-5xl font-bold capitalize">Z.</h2>
-        </span>
-        <div className="flex flex-row justify-start items-center gap-3">
-          <Link
-            href={"/"}
-            className="px-4 py-2.5 bg-blue-500 text-white font-medium text-base rounded-full"
-          >
-            Create account.
-          </Link>
-        </div>
-      </nav>
       <section className="mt-8 py-32 px-4">
         <div className="w-full h-[600px] flex px-10">
           <div className="flex flex-col w-1/2 justify-center place-items-stretch gap-4 my-4">
@@ -50,7 +34,7 @@ export default function Home() {
         <SliderMenu />
         <div className="py-12 w-full mx-auto">
           <section className="flex flex-col justify-start">
-            <div className="flex flex-wrap flex-row justify-center items-center gap-12">
+            <div className="flex flex-wrap flex-row justify-center items-center gap-8">
               <Thumbnail />
               <Thumbnail />
               <Thumbnail />
@@ -59,35 +43,6 @@ export default function Home() {
           </section>
         </div>
       </section>
-      <footer className="p-2 bg-black">
-        {/* <div className="flex flex-row justify-start items-center">
-          <section className="">
-            <h2 className="text-3xl text-white font-bold capitalize">Z.</h2>
-            <div className="border-y-[1px] border-slate-300">
-              <p className="text-sm text-slate-300 font-light capitalize">zetta space</p>
-            </div>
-          </section>
-          <section>
-            <ul>
-              <li>
-                <Link href={"/"} className="text-white">Home</Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-white">Home</Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-white">Home</Link>
-              </li>
-            </ul>
-          </section>
-          <section></section>
-        </div> */}
-        <div className="flex px-5 justify-center">
-          <p className="font-medium text-white capitalize py-3 text-xs">
-            all rights received - zetta space powered by zetta technologies
-          </p>
-        </div>
-      </footer>
     </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SideBar from "./SideBar";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <nav className="w-full px-10 py-5 bg-white shadow-md fixed top-0 flex flex-row justify-between items-center">
+      <div>
+        <SideBar />
+      </div>
+      <span>
+        <h2 className="text-5xl font-bold capitalize">Z.</h2>
+      </span>
+      <div className="flex flex-row justify-start items-center gap-3">
+        <Link
+          href={"/"}
+          className="px-4 py-2.5 bg-blue-500 text-white font-medium text-base rounded-full"
+        >
+          Create account.
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
